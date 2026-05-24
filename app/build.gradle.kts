1f711709e080d57846f51b19d66b8d82cd2466e5
dinosonicgo3 <dinosonicgo3@gmail.com>
2026-05-24 23:07:16 +0800

    Update versionCode and versionName in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.oracleairescue"
         minSdk = 26
         targetSdk = 35
-        versionCode = 76
-        versionName = "2.5.0"
+        versionCode = 78
+        versionName = "2.5.2"
     }
 
     signingConfigs {
